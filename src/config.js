8ce6df8255af6d3f6a7273041914dbd1a170960e
Israel99XD <israelsldn@gmail.com>
2024-12-09 01:44:06 -0600

    agregue url mongo

--- src/config.js
+++ src/config.js
@@ -2,4 +2,4 @@ import { config } from "dotenv";
 config();
 
 export const PORT = process.env.PORT || 4000;
-export const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost:27017/notas";
+export const MONGODB_URI = process.env.MONGODB_URI || "mongodb+srv://seriscompany:cPsPUFaEUiMoOOdb@cluster0.pnfekrf.mongodb.net/Ordenes";
